3b8879ca52c7e7af6a047d508b7186b52b745807
myusername <harun@businessaccelerate.com.bd>
2021-04-05 23:42:43 +0600

    Horizontal List View

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main(){
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Creating A GridList',
+      title: 'Creating A Horizontal List',
       home: MyApp(),
     )
   );
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget{
           )
         ],
       ),
-      body: GridWidget(),
+      body: HorizontalListWidget(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.pink,
         elevation: 30,
@@ -47,48 +47,37 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class GridWidget extends StatelessWidget{
+class HorizontalListWidget extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        print('Clicked Grid Item');
-      },
-      child:GridView.count(
-        //This line is for scrolling the horizontal
-        //scrollDirection: Axis.horizontal,
-        //Column count
-        crossAxisCount: 2,
-        children: List.generate(100,(index){
-          return Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                color: Colors.grey.withOpacity(.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset:Offset(0,3),
-              )
-              ],
-            ),
-            child : Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  'Item $index'
-                ),
-                Text(
-                  'Item $index'
-                )
-              ],
-            ),
-          );
-        }),
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      height: 200,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            width: 160.0,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.deepOrange,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.blue,
+          )
+        ],
       ),
     );
   }
 }
+
+class Product {
+  String name;
+  Product({this.name, String productName});
+}
+
