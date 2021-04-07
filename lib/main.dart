5fea2e8a64b0285cb7c7edf0391c9b99ab15cc36
myusername <harun@businessaccelerate.com.bd>
2021-04-07 23:01:13 +0600

    flutter ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,209 +1,134 @@
+
+
+
+
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(
     MaterialApp(
-      title: "ListView With Different Item",
+      title: 'App No 1',
+      theme: ThemeData(
+        primaryColor: Colors.white
+      ),
       home: MyApp(),
     )
   );
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        toolbarOpacity: 1,
-        leading: Icon(Icons.menu),
-        elevation: 10,
-        title: Text(
-          "ListView",
-          style: Theme.of(context).textTheme.headline,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-          )
-        ],
-      ),
-      body: WidgetListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: IconButton(
-          icon: Icon(Icons.add),
-          color: Colors.blue,
-        ),
-      ),
-    );
-  }
-}
 
-class WidgetListView extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
-    return WidgetListViewState();
+    // TODO: implement createState
+    return MyAppState();
   }
 }
 
-class WidgetListViewState extends State<WidgetListView>{
+class MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
-    return aCustomListView(context);
-  }
-
-
-  Widget aCustomListView(BuildContext context){
-
-    Widget container=Expanded(
-      child: Column(
-        children: <Widget>[
-          Text('harun'),
-          Text('Rashid'),
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text('App No 1'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
         ],
       ),
+      body: RandomWords(),
+      floatingActionButton: FloatingActionButton(),
+    );
+  }
+
+
+  void _pushSaved() {
+    print("tappes");
+    Navigator.of(context).push(
+        MaterialPageRoute<void>(
+            builder: (BuildContext context){
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text('Saved Suggestions'),
+                ),
+                floatingActionButton: FloatingActionButton(
+                  child: Icon(Icons.add),
+                  tooltip: "Add",
+                ),
+              );
+            }
+        )
     );
 
+  }
+}
+
+
+class RandomWords extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+
+    return RandomWordState();
+  }
+
+}
 
 
 
+
+
+
+class RandomWordState extends State<RandomWords>{
+  final suggestions= <WordPair>[];
+  final fontSize = new TextStyle(fontSize: 18);
+  final saved=<WordPair>{};
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+        child: buildSuggestions(),
+    );
+  }
+
+  Widget buildSuggestions() {
     return ListView.builder(
-      itemCount: 100,
-      itemBuilder: (context,index){
-        return Container(
-          child: Row(
-            children: <Widget>[
-              container,
-              container,
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-
-
-
-
-
-  Widget aDynamicListView(BuildContext context){
-
-    // backing data
-    // final europeanCountries = ['Albania', 'Andorra', 'Armenia', 'Austria',
-    //   'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria',
-    //   'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland',
-    //   'France', 'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland',
-    //   'Italy', 'Kazakhstan', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania',
-    //   'Luxembourg', 'Macedonia', 'Malta', 'Moldova', 'Monaco', 'Montenegro',
-    //   'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia',
-    //   'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden',
-    //   'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom', 'Vatican City'];
-
-
-    final titles = ['bike', 'boat', 'bus', 'car',
-      'railway', 'run', 'subway', 'transit', 'walk'];
-
-    final icons = [Icons.directions_bike, Icons.directions_boat,
-      Icons.directions_bus, Icons.directions_car, Icons.directions_railway,
-      Icons.directions_run, Icons.directions_subway, Icons.directions_transit,
-      Icons.directions_walk];
-
-    return ListView.separated(
-      itemCount: titles.length,
-      itemBuilder: (context,index){
-        return Container(
-          margin: EdgeInsets.all(20),
-          padding: const EdgeInsets.all(0),
-          child: Card(
-            elevation: 10,
-            child: ListTile(
-              leading: Icon(icons[index]),
-              title: Text(titles[index]),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              onTap: (){
-                print(titles[index]);
-              },
-            ),
-          ),
-        );
-        },
-      separatorBuilder: (context,indext){
-        return Divider();
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext context,int i){
+          if(i.isOdd){
+            return Divider();
+          }
+          final int index=i~/2;
+          suggestions.addAll(generateWordPairs().take(10));
+          return buildRow(suggestions[index]);
       },
 
     );
   }
 
-
-
-  Widget aSimpleListView(BuildContext context) {
-    return ListView(
-      children: ListTile.divideTiles(
-        context: context,
-        color: Colors.black,
-        tiles : [
-        ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Green'),
-        ),  ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Green'),
-        ),  ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Green'),
-        ),  ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Green'),
-        ),  ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Green'),
-        ),  ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Green'),
-        ),  ListTile(
-          title: Text('Sun'),
-        ),
-        ListTile(
-          title: Text('Moon'),
-        ),
-        ListTile(
-          title: Text('Green'),
-        ),
-      ],
-      ).toList(),
+  Widget buildRow(WordPair pair) {
+    final alreadySaved=saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: fontSize,
+      ),
+      trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            saved.remove(pair);
+          }else{
+            saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
